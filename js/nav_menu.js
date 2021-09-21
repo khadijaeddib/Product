59ba1e9af297736d3eaa2fcbc85eb7cf4f0edf27
khadija eddib <khadijaeddib05@gmail.com>
2021-09-22 00:58:15 +0100

    13-fix header for mobile

--- js/nav_menu.js
+++ js/nav_menu.js
@@ -1,6 +1,5 @@
 const menuButton = document.querySelector('.main-header-nav-btn')
-const HeaderNavCollapse =  document.querySelector('.collapse-nav')
-const HeaderNavCollapseItems =  document.querySelector('.collapse-nav-items')
+const HeaderNavCollapse =  document.querySelector('.main-header-nav')
 
 let showMenu = false;
 
@@ -9,19 +8,17 @@ menuButton.addEventListener('click', toggleMenu)
 function toggleMenu() {
     if (!showMenu) {
         HeaderNavCollapse.classList.add('open-menu')
-        HeaderNavCollapseItems.classList.add('open-menu-items')
 
-        HeaderNavCollapseItems.style.animation = 'showMenu 1s'
+        HeaderNavCollapse.style.animation = 'showMenu 1s'
         
         showMenu = true;
 
     } else {
-        HeaderNavCollapseItems.style.animation = 'hideMenu 1s'
+        HeaderNavCollapse.style.animation = 'hideMenu 1s'
 
         setTimeout(function(){ 
             HeaderNavCollapse.classList.remove('open-menu')
-            HeaderNavCollapseItems.classList.remove('open-menu-items')
-        }, 700)
+        }, 400)
         
         showMenu = false
     }
