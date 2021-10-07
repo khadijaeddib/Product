31b6a96d16fb6fba41af24a6f0620f53e2b4b6ef
khadija eddib <khadijaeddib05@gmail.com>
2021-10-07 22:34:28 +0100

    33-modal video added

---
+++ js/modal-video.js
@@ -0,0 +1,38 @@
+const playButton = document.querySelector('.play-btn')
+const modal = document.querySelector('.modal')
+const modalContent = document.querySelector('.modal-content')
+const video = document.querySelector('.modal-content iframe')
+const closeButton = document.querySelector('.close-btn')
+
+
+function showModal () {
+    video.src="https://www.youtube.com/embed/N5wpD9Ov_To"
+    modal.style.visibility='visible'
+    video.style.animation = "showModalAnimation .6s"
+}
+
+function hideModal () {
+    video.src = ''
+    video.style.animation = "hideModalAnimation .4s ease-out"
+    setTimeout(function() {modal.style.visibility='hidden'}, 400)
+}
+
+function windowClick(event) {
+    if (event.target == modal) {
+        hideModal ()
+    }
+    if (event.target == modalContent) {
+        hideModal ()
+    }
+}
+
+function escapeClick(e) {
+    if (e.key === "Escape" || e.key === "Esc") {
+        hideModal ()
+    }
+}
+
+playButton.addEventListener('click',showModal)
+closeButton.addEventListener('click',hideModal)
+window.addEventListener('click', windowClick)
+window.addEventListener('keydown', escapeClick)
